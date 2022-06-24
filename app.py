fb7821c056a8fe576313ff98f100f22c39094939
Aniass <a.staskiewicz4@gmail.com>
2022-06-24 15:20:53 +0200

    adding project

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,  request
+from joblib import load
+
+with open('C:\\Users\\PC\\Git_projects\\SentimentApp\\models\\LR_model.pkl', 'rb') as file:
+    model = load(file)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/result', methods=['POST'])
+def get_result():
+    if request.method == 'POST':
+        input_text = request.form['text']
+        data = [input_text]
+        result = model.predict(data)
+        if int(result) == 1:
+            my_prediction = "This review is positive"
+        else:
+            my_prediction = "This review is negative"
+        return render_template("result.html", prediction=my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
